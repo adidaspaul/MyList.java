46da4d438a1b0c7c031bec095c6a802b36078c6f
Paul <quazantip@gmail.com>
2021-12-02 20:29:26 +0400

    Work Around NPE.

--- src/main/java/hw_8/MyQueue.java
+++ src/main/java/hw_8/MyQueue.java
@@ -1,84 +1,98 @@
 package hw_8;
 
-//create queue
-public class MyQueue {
-    public static int size;
-    public static Object[] queue;
-    private static int length;
-    private static int MAX_SIZE = Integer.MAX_VALUE;
+public class MyQueue<E> {
+    private int size = 0;
+    private Node<E> first;
+    private Node<E> last;
 
-    public boolean add(Object value) {
-        try {
-            if (size >= length - 1) {
-                grow();
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        public Node(E item, Node<E> next, Node<E> prev) {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(item);
+        }
+    }
+
+    public void add(E value) {
+        Node<E> l = last;
+        Node<E> newNode = new MyQueue.Node<>(value, null, l);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+    }
+
+    public void remove(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Provided index is out of bounds of the size " + size);
+        }
+        Node<E> previous = null;
+        Node<E> nextious = null;
+        Node<E> look = first;
+        if (index == 0) {
+            first = look.next;
+        } else if (index == size - 1) {
+            last = look.prev;
+        } else {
+            int counter = 0;
+            while (counter <= index + 1) {
+                if (counter == index - 1) {
+                    previous = look;
+                }
+                if (counter == index + 1) {
+                    nextious = look;
+                }
+                look = look.next;
+                counter++;
             }
-            queue[size] = value;
-            size++;
-            return true;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            previous.next = nextious;
+            nextious.prev = previous;
         }
-        return false;
-    }
-
-    public int size() {
-
-        return size;
-    }
-
-    public static Object remove(int index) {
-        if (index < 0 || size <= 0) {
-            return null;
-        }
-        Object result = queue[index];
-        if (length - index >= 0) System.arraycopy(queue, index + 1, queue, index, length - index - 1);
         size--;
-        if (size <= length / 2) {
-            grow();
+        if (size == 0) {
+            first = null;
+            last = null;
         }
-        return result;
     }
 
     public void clear() {
-        this.queue = new Object[10];
-        this.length = 10;
-        this.size = 0;
+        size = 0;
+        first = null;
+        last = null;
     }
 
-    public Object peek() {
-        return queue[0];
+    public int size() {
+        return size;
     }
 
-    public Object poll(){
-        Object result;
-        result = MyQueue.remove(0);
+    public E peek(){
+        return first.item;
+    }
+
+    public E poll(){
+        if(size == 0){
+            return null;
+        }
+        Node<E> newFirst = first.next;
+        Node<E> removable = first;
+        first = newFirst;
         size--;
-        return result;
+        if(size == 0){
+            last = null;
+        }
+        return removable.item;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder result = new StringBuilder("MyQueue {size=" + size + "; length=" + length + "; ");
-        for (int i = 0; i < size; i++) {
-            result.append(queue[i].toString());
-            result.append(", ");
-        }
-        result.deleteCharAt(result.length() - 1);
-        result.deleteCharAt(result.length() - 1);
-        result.append("}");
-        return result.toString();
-    }
-
-    private static void grow() throws IllegalArgumentException {
-        if (size >= MAX_SIZE) {
-            throw new IllegalArgumentException("It can't grow anymore.");
-        }
-        length = size + size / 2 + 1;
-        if (length < size) {
-            length = MAX_SIZE;
-        }
-        Object[] newArray = new Object[length];
-        System.arraycopy(queue, 0, newArray, 0, size);
-        queue = newArray;
-    }
 }
